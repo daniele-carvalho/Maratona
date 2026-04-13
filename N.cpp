24428ec4308952901ac37d7127b45bc08be8f722
Daniele Carvalho <danielehitashi21@gmail.com>
2026-04-13 20:30:37 -0300

    add N

---
+++ N.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int t; cin>>t;
+
+    for (int i=0;i<t;i++){
+        int x; cin>>x;
+
+        for (int j=1;j<=x;j++){
+            cout<<j*j<<endl;
+        }
+    }
+}
